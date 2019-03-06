f6ce296eaa2ffd58e5c821133e0ccc19fb595582
Panni <simaianna3@gmail.com>
2019-03-06 10:24:16 +0100

    working js no css

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+function checkCountry() {
+  let countrycode = document.querySelector("#country").value;
+  console.log(`Checking the code ${countrycode}`);
+
+  fetch(`https://restcountries.eu/rest/v2/callingcode/${countrycode}`)
+    .then(response => response.json())
+    .then(countries => {
+      printCountry(countries);
+      // console.log(`The country is ${countries[0]["name"]}`);
+      // console.log(`The country is ${countries[1]["name"]}`);
+    });
+}
+
+function printCountry(data) {
+  const country = document.querySelector(".print");
+  const content = data
+    .map(function(myCountry) {
+      const { name, capital, flag, population, region } = myCountry;
+      const img = `<img src=${flag}>`;
+      const infos = `<h1>${name}</h1>
+      <img src=${flag}>`;
+      country.innerHTML = infos;
+    })
+    .join("");
+}
